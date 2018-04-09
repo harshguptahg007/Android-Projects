79ecb8ab71e136083f20ff1f37fdf481029c10f0
harshguptahg007 <harshguptahg007@gmail.com>
2018-04-09 23:31:50 +0530

    Pets App

---
+++ ud845-Pets-starting-point/app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -0,0 +1,53 @@
+package com.example.android.pets;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.pets.data.PetContract;
+
+/**
+ * Created by harsh on 5/4/18.
+ */
+
+public class PetCursorAdapter extends CursorAdapter {
+
+    public PetCursorAdapter(Context context, Cursor c) {
+        super(context, c,0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item,parent,false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        //Find Individual views that we want to modify in the list item layout
+        TextView nameTextView=(TextView)view.findViewById(R.id.name);
+        TextView summaryTextView=(TextView)view.findViewById(R.id.summary);
+
+        //Find the columns of the Pet Attribute that we are interested in
+        int nameColumnIndex=cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_NAME);
+        int breedColumnIndex=cursor.getColumnIndex(PetContract.PetEntry.COLUMN_PET_BREED);
+
+        //Read the pet attributes from the Cursor for the Current pet
+        String petName=cursor.getString(nameColumnIndex);
+        String petBreed=cursor.getString(breedColumnIndex);
+
+        if (TextUtils.isEmpty(petBreed)) {
+            petBreed = context.getString(R.string.unknown_breed);
+        }
+
+        //Update the textviews with the attributes for the current pet
+        nameTextView.setText(petName);
+        summaryTextView.setText(petBreed);
+
+    }
+}
